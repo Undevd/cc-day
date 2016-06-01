af6f2ad4c39def1899bdc5a3b244e51458d427e4
Scott Hulme <scott.r.hulme@live.co.uk>
2016-06-02 00:02:34 +0100

    Adding view changes and model

--- public/app/register/formCtrl.js
+++ public/app/register/formCtrl.js
@@ -1,8 +1,23 @@
 angular.module('myApp').controller('formCtrl', function($scope) {
     $scope.page = 'Welcome',
     
-    $scope.customer = [],
-    $scope.customer.name = ""
+    $scope.name = "",
+    $scope.contactnumber = "",
+    $scope.emailaddr = "",
+    $scope.nextofkin = "",
+    $scope.nextofkincontactnumber = "",
+    $scope.howdidyouhear = "",
+    $scope.questadults = 0,
+    $scope.questkids = 0,
+    $scope.questmedical = "",
+    $scope.walkadults = 0,
+    $scope.walkkids = 0,
+    $scope.activitytotalcost = (($scope.questadults + $scope.questkids) * 10) + ($scope.walkadults * 5) + ($scope.walkkids * 2.5),
+    $scope.transportrequired = false,
+    $scope.transporthowmany = 0,
+    $scope.lunchadults = 0,
+    $scope.lunchkids = 0,
+    $scope.lunchdietaryreqs = "",
     
     $scope.setPage = function(pageName) {
         console.log(pageName);
