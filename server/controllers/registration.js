8d61f0144730c3b58b1cbf7c53ae0eb99c25a158
Scott Hulme <scott.r.hulme@live.co.uk>
2016-06-12 21:20:13 +0100

    Updating registration e-mail template

--- server/controllers/registration.js
+++ server/controllers/registration.js
@@ -57,6 +57,59 @@ exports.createRegistration = function(request, response) {
             }
         });
 
+        
+        sparky.transmissions.send({
+            transmissionBody: {
+                content: {
+                from: 'registration@' + process.env.SPARKPOST_SANDBOX_DOMAIN, // 'testing@sparkpostbox.com'
+                subject: 'Registration Received',
+                html:`<html>
+                        <body>
+                            <p>A registration has been received:</p>
+                            <h4>Details</h4>
+                            <ul>
+                                <li>Name: ` + registrationData.name + `</li>
+                                <li>Contact Number: ` + registrationData.contact_number + `</li>
+                                <li>E-mail: ` + registrationData.email_addr + `</li>
+                                <li>Next of kin: ` + registrationData.next_of_kin + `</li>
+                                <li>Next of kin contact number: ` + registrationData.next_of_kin_contact_number + `</li>
+                                <li>How did you hear: ` + registrationData.how_did_you_hear + `</li>
+                            </ul>
+                            <h4>Activities</h4>
+                            <ul>
+                                <li>Quest Adults: ` + registrationData.activities_quest_adults + `</li>
+                                <li>Quest Kids: ` + registrationData.activities_quest_kids + `</li>
+                                <li>Quest Medical: ` + registrationData.activities_quest_medical + `</li>
+                                <li>Walk Adults: ` + registrationData.activities_walk_adults + `</li>
+                                <li>Walk Kids: ` + registrationData.activities_walk_kids + `</li>
+                                <li>Total Cost: ` + registrationData.activities_totalcost + `</li>
+                            </ul>
+                            <h4>Transport</h4>
+                            <ul>
+                                <li>Transport Required: ` + registrationData.transport_required + `</li>
+                                <li>Transport How Many: ` + registrationData.transport_howmany + `</li>
+                            </ul>
+                            <h4>Lunch</h4>
+                            <ul>
+                                <li>Lunch Adukts: ` + registrationData.lunch_adults + `</li>
+                                <li>Lunch Kids: ` + registrationData.lunch_kids + `</li>
+                                <li>Dietary Requirements: ` + registrationData.lunch_dietaryreqs + `</li>
+                            </ul>
+                        </body>
+                    </html>`
+                },
+                recipients: [
+                {address: process.env.REGISTRATION_INBOX}
+                ]
+            }
+            }, function(err, res) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Registration e-mail sent to: ' + process.env.REGISTRATION_INBOX);
+            }
+        });
+
         //Set the success status and send the new registration code
         response.status(201).send({code: code});
 
