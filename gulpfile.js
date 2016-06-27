316bd9ffff6655c3eebcdea65d998b225f647943
Scott Hulme <shulme@civica.co.uk>
2016-06-27 13:25:03 +0100

    Adding multiple changes

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ gulp.task('start', function () {
   nodemon({
     script: 'server.js'
   , ext: 'js html'
-  , env: { 'NODE_ENV': 'development' }
+  , env: { 'NODE_ENV': 'development'}
   });
 });
 
